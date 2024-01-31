99d307a0535da6e18feb818191dd091b9733af5e
EmanuelePiemontese <e.piemontese@studenti.uniba.it>
2024-01-31 13:43:57 +0100

    Aggiunta della sezione account

--- app/src/main/java/it/uniba/dib/piu/softwarechasers/fitnessapp/ui/account/AccountFragment.java
+++ app/src/main/java/it/uniba/dib/piu/softwarechasers/fitnessapp/ui/account/AccountFragment.java
@@ -1,15 +1,22 @@
 package it.uniba.dib.piu.softwarechasers.fitnessapp.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import it.uniba.dib.piu.softwarechasers.fitnessapp.R;
+import it.uniba.dib.piu.softwarechasers.fitnessapp.access.LoginFragment;
+import it.uniba.dib.piu.softwarechasers.fitnessapp.access.LoginSignupActivity;
 import it.uniba.dib.piu.softwarechasers.fitnessapp.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
@@ -24,9 +31,18 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button logout = root.findViewById(R.id.sign_out);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), LoginSignupActivity.class));
+                getActivity().finish();
+            }
+        });
+
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
